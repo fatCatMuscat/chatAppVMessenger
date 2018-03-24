6b75128fa8495a1236914a1d2d165fd998ca7a20
arsenal <strigunovqa@gmail.com>
2018-03-24 14:17:08 -0700

    initial commit

--- app/src/main/java/com/londonappbrewery/flashchatnewfirebase/LoginActivity.java
+++ app/src/main/java/com/londonappbrewery/flashchatnewfirebase/LoginActivity.java
@@ -1,18 +1,14 @@
 package com.londonappbrewery.flashchatnewfirebase;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 
