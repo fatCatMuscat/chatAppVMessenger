a5b2206302bd17739d53aa29af11a223bd4c279c
arsenal <strigunovqa@gmail.com>
2018-03-24 18:41:19 -0700

    added registration functionality

--- app/src/main/java/com/fatcatmuscat/VMessenger/RegisterActivity.java
+++ app/src/main/java/com/fatcatmuscat/VMessenger/RegisterActivity.java
@@ -1,14 +1,25 @@
 package com.fatcatmuscat.VMessenger;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -24,8 +35,8 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText mPasswordView;
     private EditText mConfirmPasswordView;
 
-    // Firebase instance variables
-
+    // FireBase instance variables
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -51,7 +62,7 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
         // TODO: Get hold of an instance of FirebaseAuth
-
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -97,7 +108,7 @@ public class RegisterActivity extends AppCompatActivity {
             focusView.requestFocus();
         } else {
             // TODO: Call create FirebaseUser() here
-
+            createFirebaseUser();
         }
     }
 
@@ -107,19 +118,68 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Add own logic to check for a valid password (minimum 6 characters)
-        return true;
+        String confirmPassword = mConfirmPasswordView.getText().toString();
+        return confirmPassword.equals(password) && password.length() > 6;
     }
 
     // TODO: Create a Firebase user
+    private void createFirebaseUser() {
 
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        // *** LOG MESSAGE ***
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d("VMessenger", "createUser onComplete: " + task.isSuccessful());
+
+                if(!task.isSuccessful()) {
+                    Log.d("VMessenger", "user creation failed");
+                    showErrorDialogue("Registration attempt failed");
+                }
+                else {
+                    saveDisplayName();
+                   // showUserRegisteredDialog("Registration attempt successful");
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    finish();
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+    }
 
     // TODO: Save the display name to Shared Preferences
+    private void saveDisplayName() {
+        String displayName = mUsernameView.getText().toString();
+        SharedPreferences prefs = getSharedPreferences(CHAT_PREFS, 0);
+        prefs.edit().putString(DISPLAY_NAME_KEY, displayName).apply();
 
+    }
 
     // TODO: Create an alert dialog to show in case registration failed
 
+    private void showErrorDialogue(String message) {
 
+        new AlertDialog.Builder(this)
+                .setTitle("Ouch!")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok,null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
 
+    }
+
+    private void showUserRegisteredDialog(String message) {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Congratz! Registration successful!")
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+    }
 
 }
