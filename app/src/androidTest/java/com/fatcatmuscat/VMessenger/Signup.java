bfe7968939d6574d317694a2448a17ff7f11ce05
arsenal <strigunovqa@gmail.com>
2018-03-29 08:33:04 -0700

    added 1st running test

--- app/src/androidTest/java/com/fatcatmuscat/VMessenger/Signup.java
+++ app/src/androidTest/java/com/fatcatmuscat/VMessenger/Signup.java
@@ -2,10 +2,13 @@ package com.fatcatmuscat.VMessenger;
 
 import android.support.test.espresso.ViewAssertion;
 import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -17,10 +20,13 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
  * Created by arsenal on 3/28/18.
  */
 
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class Signup {
 
 
     @Rule
+    public final
     ActivityTestRule<RegisterActivity> mActivityActivityTestRule =
             new ActivityTestRule<>(RegisterActivity.class);
 
