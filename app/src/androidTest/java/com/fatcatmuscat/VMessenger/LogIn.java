09761cb6d9dd4d72de813e6661a186315ea99b90
arsenal <strigunovqa@gmail.com>
2018-04-05 16:26:34 -0700

    added LogIn class for login tests

---
+++ app/src/androidTest/java/com/fatcatmuscat/VMessenger/LogIn.java
@@ -0,0 +1,8 @@
+package com.fatcatmuscat.VMessenger;
+
+/**
+ * Created by arsenal on 4/5/18.
+ */
+
+public class LogIn {
+}
--- app/src/androidTest/java/com/fatcatmuscat/VMessenger/Signup.java
+++ app/src/androidTest/java/com/fatcatmuscat/VMessenger/Signup.java
@@ -14,8 +14,10 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -40,6 +42,16 @@ public class Signup {
     @Before
     public void setup() {
         resources = InstrumentationRegistry.getTargetContext().getResources();
+    }
+
+    @Test
+    public void testBackButton() {
+        onView(withId(R.id.register_sign_up_button))
+                .perform(click());
+        onView(withId(R.id.register_username))
+                .perform(typeText("BobTheGoose"));
+        closeSoftKeyboard();
+
     }
 
 
